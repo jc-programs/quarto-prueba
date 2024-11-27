3fa4bcfc20baf8541311f46207d2d458ad264ebd
jc-programs <jose@gueto.com>
2024-11-27 17:53:56 +0100

    publish_quarto.sh v1

--- publish_quarto.sh
+++ publish_quarto.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# check if log directory exists
+if [ ! -d "log" ]; then
+    # if not then we try to create it
+    mkdir log
+    if [ $? -ne 0 ] ; then
+        echo "log directory does not exist and can't be created."
+        exit 1
+    fi
+fi
+
 LOG_FILE=log/$(date +"%Y-%m-%d").log
 
 log_message() {
     local type="$1"
     local message="$2"
     local timestamp=$(date +"%H:%M:%S")
-    echo "[${timestamp}] ${type} : ${message}" | tee -a "${LOG_FILE}".txt
+    echo "[${timestamp}] ${type}: ${message}" | tee -a "${LOG_FILE}".txt
 }
 
 log_message_error() { 
@@ -16,8 +26,6 @@ log_message_error() {
 log_message_info() {
     log_message "INFO" "$1"
 }
-
-BRANCH_MASTER=$(git branch --show-current)
 
 # Check if the commit message is provided
 if [ -z "$1" ]; then
@@ -33,14 +41,57 @@ if [ $# -gt 1 ]; then
     exit 1
 fi
 
+# get current branch
+BRANCH=$(git branch --show-current 2>/dev/null)
+if [ $? -ne 0 ]; then
+    log_message_error "git branch --show-current"
+    exit 1
+fi
 
+# Check if there are some changes to save
+# https://stackoverflow.com/questions/3878624/how-do-i-programmatically-determine-if-there-are-uncommitted-changes
+GS=$(git status --porcelain=v1 2>/dev/null)
+if [ $? -eq 128 ]; then
+    log_message_error "There is no git repository in current folder."
+    log_message_error "Current folder is: $(pwd)"
+    exit 1
+fi
 
-# # Commit the changes with the provided message
-# git add .
-# git commit -m "$1"
+if [ -z "${GS}" ]; then
+    log_message_info "There are no changes to commit."
+    exit 0
+fi
 
-# # Push the changes to the main branch
-# git push origin main
+git add . 2>/dev/null
+if [ $? -ne 0 ]; then
+    log_message_error "git add ."
+    exit 1
+else
+    log_message_info "git add ."
+fi
 
-# # Publish to GitHub Pages using quarto
-# quarto publish gh-pages --no-render --no-prompt
+git commit -m "$1" 2>/dev/null
+if [ $? -ne 0 ]; then
+    log_message_error "git commit -m \"$1\""
+    exit 1
+else
+    log_message_info "git commit -m \"$1\""
+fi
+
+git push origin $BRANCH
+if [ $? -ne 0 ]; then
+    log_message_error "git push origin $BRANCH"
+    exit 1
+else
+    log_message_info "git push origin $BRANCH"
+fi
+
+quarto publish gh-pages --no-prompt
+if [ $? -ne 0 ]; then
+    log_message_error "quarto publish gh-pages --no-prompt"
+    exit 1
+else
+    log_message_info "quarto publish gh-pages --no-prompt"
+fi
+
+exit 0
